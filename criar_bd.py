194d731f3364e66c1d2f3a9530035f8b393966bf
williamlopesdev <wilcypherr@gmail.com>
2022-09-19 09:53:31 -0300

    primeiro commit:  criando conexão com bd - sql lite

---
+++ criar_bd.py
@@ -0,0 +1,11 @@
+
+import sqlite3 as lite
+
+#criando conexão
+con = lite.connect('dados.db')
+
+#criando tabela
+with con:
+    cur=con.cursor()
+    cur.execute("CREATE TABLE inventorio(id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, local TEXT, descricao TEXT, marca TEXT, data_da_compra DATE, valor_da_compra DECIMAL, serie TEXT, imagem TEXT)")
+    
